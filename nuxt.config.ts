c01e81a93050e2fde78572db0eaf116ae552c701
Yue Yu <yuyueshihaoren@gmail.com>
2023-10-14 21:39:24 -0700

    added app info

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@vueuse/nuxt"],
   ssr: false,
+  app: {
+    baseURL: "/pourover-guide/",
+    buildAssetsDir: "assets",
+  },
 });
